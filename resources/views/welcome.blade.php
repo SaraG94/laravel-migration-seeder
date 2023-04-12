53c1a23a11c6a258b087dc58233bd110ced19b7c
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-12 18:34:48 +0200

    FEAT content in welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,26 @@
 <body>
 
     <main class="bg-light">
-       
+
+        <div class="container">
+            <div class="row">
+                @forelse ($trains as $train)
+                    <div class="col-3">
+                        <div class="card">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">{{$train->azienda}}</li>
+                                <li class="list-group-item">Codice Treno: {{$train->codice_treno}}</li>
+                                <li class="list-group-item">Da: {{$train->stazione_di_partenza}}</li>
+                                <li class="list-group-item">A: {{$train->stazione_di_arrivo}}</li>
+                                <li class="list-group-item">Partenza: {{$train->orario_di_partenza}}</li>
+                                <li class="list-group-item">Arrivo: {{$train->orario_di_arrivo}}</li>
+                            </ul>
+                        </div>
+                    </div>
+                @empty
+                @endforelse
+            </div>
+        </div>
     </main>
 
 </body>
