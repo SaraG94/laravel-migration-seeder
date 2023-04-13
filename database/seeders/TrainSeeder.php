c428cb84caa1250d3344f9d8f85eb8934e6ad6d6
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-13 16:47:11 +0200

    FIX seeder faker

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
 use Faker\Generator as Faker;
+use Carbon\Carbon;
 
 class TrainSeeder extends Seeder
 {
@@ -19,18 +20,21 @@ class TrainSeeder extends Seeder
         $aziende = ['trenitalia', 'italo', 'trenord'];
 
         for ($i = 0; $i < 10; $i++) {
-            $partenze = $faker->unixTime();
+            $partenze = ($today = Carbon::today());
+            // $newDate = new DateTime ($partenze);
+            // $newDate->add('P1H');
+            $arrivi = ($tomorrow = Carbon::tomorrow());
 
             $new_train = new Train();
             $new_train->azienda = $faker->randomElement($aziende);
             $new_train->stazione_di_partenza = $faker->city();
             $new_train->stazione_di_arrivo = $faker->city();
             $new_train->orario_di_partenza = $partenze;
-            $new_train->orario_di_arrivo = $faker->unixTime(new $partenze('+1 hour'));
-            $new_train->codice_treno = bothify('??-######');
-            $new_train->numero_carrozze = numberBetween(2, 15);
+            $new_train->orario_di_arrivo = $arrivi;
+            $new_train->codice_treno = $faker->bothify('??-######');
+            $new_train->numero_carrozze = $faker->numberBetween(2, 15);
                        
-            $new_house->save();
+            $new_train->save();
         }
     }
 }
